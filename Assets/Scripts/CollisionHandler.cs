d043a300f7539777a1d8ed8ce9ad5130668ebab2
Chris Kirby <chriskirbygithub@gmail.com>
2022-03-27 17:09:02 +0100

    create explosions, update collision script update assets and made self destruct script
    created player and enemy explosions when hit, updated assets, installed effect examples and made a selfdestruct script to remove cloned explosions

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,18 +4,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] float levelLoadDelay = 1f;
-     private void OnTriggerEnter(Collider other) 
+    [SerializeField] ParticleSystem crashVFX;
+     private void OnTriggerEnter(Collider other)
     {
         StartCrashSequence();
     }
 
     private void StartCrashSequence()
     {
+        crashVFX.Play();
         GetComponent<PlayerControls>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
         Invoke("ReloadLevel", levelLoadDelay);
+
     }
 
     void ReloadLevel()
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
     [Tooltip("The amount of time for the enemy to be destroyed after being hit")] 
     [SerializeField] float destroyTime = 0.5f;
     private void OnParticleCollision(GameObject other) 
     {
-        Debug.Log(this.name + "--I'm hit by--" + other.gameObject.name);
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject, destroyTime);
     }
 }
