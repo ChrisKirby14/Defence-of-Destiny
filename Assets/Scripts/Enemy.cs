cf4588f5d7a06c656218e7623e9acddc8c138e5e
Chris Kirby <chriskirbygithub@gmail.com>
2022-04-20 20:05:30 +0100

    create enemy prefabs and waves, add image and sounds

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,14 +4,58 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] GameObject deathVFX;
-    [SerializeField] Transform parent;
+    [Tooltip("Place your explosion VFX here, will play on enemies death")]
+    [SerializeField] GameObject deathFX;
+    [SerializeField] GameObject hitVFX;
     [Tooltip("The amount of time for the enemy to be destroyed after being hit")] 
     [SerializeField] float destroyTime = 0.5f;
-    private void OnParticleCollision(GameObject other) 
+    [SerializeField] int scorePerHit = 25;
+    [SerializeField] int scorePerDeath = 25;
+    [Header("Enemy Health")]
+    [Tooltip("Change value to increade enemies health")]
+    [SerializeField] int hitPoints = 100;
+    int damage = 25;
+
+    ScoreBoard scoreBoard;
+    GameObject parentGameObject;
+
+
+    void Start()
     {
-        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
-        Destroy(gameObject, destroyTime);
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        //Don't use FindObjectOfType in update very heavy to use
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+        AddRigidbody();
     }
+
+    void AddRigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        KillEnemy();
+    }
+
+    void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        hitPoints = hitPoints - damage;
+        vfx.transform.parent = parentGameObject.transform;
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+    void KillEnemy()
+    {
+        if (hitPoints < 1)
+        {
+            GameObject vfx = Instantiate(deathFX, transform.position, Quaternion.identity);
+            vfx.transform.parent = parentGameObject.transform;
+            scoreBoard.IncreaseScore(scorePerDeath);
+            Destroy(gameObject, destroyTime);
+        }
+    }
+    
 }
